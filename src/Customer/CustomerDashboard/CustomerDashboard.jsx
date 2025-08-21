88e23cf1cadc111c74b48d49251bcfd65b9875a5
BBoyeon <2boyeon2@gmail.com>
2025-08-21 18:37:32 +0900

    손님 메인 페이지

--- src/Customer/CustomerDashboard/CustomerDashboard.jsx
+++ src/Customer/CustomerDashboard/CustomerDashboard.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
+// src/pages/CustomerDashboard.jsx
+import React, { useEffect, useState } from 'react'
 import './CustomerDashboard.css'
-import AppHeader from '../CustomerAppHeader/AppHeader';
-import { useNavigate } from 'react-router-dom'
+import AppHeader from '../CustomerAppHeader/AppHeader'
 
 const CustomerDashboard = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: '손님',
+    gender: '남자',
+    intro: '',
+    profile: '/character/남자캐릭터.png',
+  })
+
+  useEffect(() => {
+    const name = localStorage.getItem('nickname') || '손님'
+    const gender = localStorage.getItem('gender') || '남자'
+    const intro = localStorage.getItem('introduce') || ''
+    const profile = localStorage.getItem('character') || '/character/남자캐릭터.png'
+    setUserInfo({ name, gender, intro, profile })
+  }, [])
+
   return (
     <div className="customer-dashboard">
-      <AppHeader />
-      <p>dd</p>
+      <AppHeader/>
+      <div className="dashboard-container">
+        <div className="dashboard-userinfo">
+          <img src={userInfo.profile} alt="프로필" className="profile-img" />
+          <div className="user-greeting">
+            {userInfo.name} {userInfo.gender === '여자' ? '낭자' : '도령'} ! <p>어서오시오 ~</p>
+          </div>
+          <p className="user-intro">{userInfo.intro}</p>
+        </div>
+
+        <div className="dashboard-map">
+          <div className="map-placeholder">지도 영역 (추후 GPS 적용)</div>
+        </div>
+
+        <div className="dashboard-banner">
+          <img src="/ㅇ.png" alt="광고 배너 자리" className="banner-img" />
+        </div>
+
+      </div>
     </div>
   )
 }
