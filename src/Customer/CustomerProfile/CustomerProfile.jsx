2ce1672363adcb1d84a22ce42a64f2874702e728
BBoyeon <2boyeon2@gmail.com>
2025-08-22 00:54:03 +0900

    CustomerProfile 틀 완성

--- src/Customer/CustomerProfile/CustomerProfile.jsx
+++ src/Customer/CustomerProfile/CustomerProfile.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './CustomerProfile.css'
-import AppHeader from '../CustomerAppHeader/AppHeader';
+import AppHeader from '../CustomerAppHeader/AppHeader'
 
 const CustomerProfile = () => {
   return (
-    <div className='customer-profile'>
-        <AppHeader activeMenu="profile" />
+    <div className="customer-profile">
+      <AppHeader />
+      <div className="profile-underheader">
+        <div className="profile-grid">
+          <div className="profile-card-badge">나의 뱃지 확인하는 곳</div>
+          <div className="profile-card-exchange">포인트 교환 페이지</div>
+          <div className="profile-card-point">
+            나의 포인트 확인하는 곳<br/>
+            (밑에는 어떤 미션으로 얼마나 받았는지 확인할 수 있도록 함)
+          </div>
+          <div className="profile-card-market">나의 마실 나눔 기록들 확인하기</div>
+        </div>
+      </div>
     </div>
   )
 }
